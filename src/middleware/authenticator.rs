ef8e8ac4d188c29538c7c879b69a2683a826a2e4
jailbreak <jailbreak@aliyun.com>
2023-07-05 20:25:59 +0800

    update middleware

--- src/middleware/authenticator.rs
+++ src/middleware/authenticator.rs
@@ -1,12 +1,9 @@
 use axum::{http::{Request}, middleware::Next, response::Response};
 
 
-
-
-
-
 pub async fn authenticator<B>(request: Request<B>, next: Next<B>, ) -> Response {
-    let uri = request.uri().path();
-    println!("{}",uri);
-    next.run(request).await
+    let path = request.uri().path();
+    println!("{}", path);
+    let response = next.run(request).await;
+    response
 }
--- src/router.rs
+++ src/router.rs
@@ -8,6 +8,7 @@ use crate::{common::{AppState, Responder}, handle, middleware::authenticator};
 
 pub fn create_router(app_state: AppState) -> Router {
     Router::new()
+        .route("/", get(index))
         .nest("/user", Router::new()
             .route("/all", get(handle::get_all_user)) 
             .route("/get/:username", get(handle::get_user_by_username))
@@ -17,6 +18,10 @@ pub fn create_router(app_state: AppState) -> Router {
     .with_state(app_state)
     .fallback(fallback)
            
+}
+
+async fn index() -> Responder<String> {
+    Responder::new(StatusCode::OK, "index".to_string(), "".to_string())
 }
 
 async fn fallback(uri: Uri) -> Responder<String> {
